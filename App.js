ecac50135e894ff960d7b0e00e70eb6acb8744da
Shiraz Ali <32427100+asl0007@users.noreply.github.com>
2021-03-02 12:21:01 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import MainScreen from './screens/MainScreen/MainScreen';
+import DetailScreen from './screens/DetailScreen/DetailScreen';
+import { StatusBar } from 'react-native';
+
+const AppNavigator = createStackNavigator({
+  MainScreen: {
+    screen: MainScreen,
+  },
+  DetailScreen: {
+    screen: DetailScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barstyle="light-content" />
+      <AppContainer />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
